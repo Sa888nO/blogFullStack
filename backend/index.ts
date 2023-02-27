4fc8563cdf79d6b5a67f1e67bb0eb88e729fdb64
Sa888nO <shura.reno.97@bk.ru>
2023-02-27 16:55:41 +0300

    init backend with deps

---
+++ backend/index.ts
@@ -0,0 +1,8 @@
+require('dotenv').config()
+import express from 'express'
+
+const app = express();
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => console.log(`Running on port ${port}`));
